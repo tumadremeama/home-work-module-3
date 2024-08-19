2114d28e89cfda40d71b4f6cdb51e00ecefa3992
Demyan k. <demiandiaz.dd@gmail.com>
2024-08-19 22:24:36 +0300

    Update modul 3 (global.).py

--- modul 3 (global.).py
+++ modul 3 (global.).py
@@ -1,10 +1,13 @@
 calls = 0
+
 def count_calls():
     global calls
     calls += 1
+
 def string_info(string):
     count_calls()
     return(len(string), string.upper(), string.lower())
+
 def is_contains(string, list_to_search):
     count_calls()
     string_lower = string.lower()
@@ -14,4 +17,5 @@ print(string_info('Capybara'))
 print(string_info('Armageddon'))
 print(is_contains('Urban', ['ban', 'baNaN', 'urBAN']))
 print(is_contains('cycle', ['recycling', 'cyclic']))
+
 print(calls)
