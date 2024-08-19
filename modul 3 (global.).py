f9963e01513aca6f7529bbf091b06f6118e2f5e8
Demyan k. <demiandiaz.dd@gmail.com>
2024-08-19 22:22:49 +0300

    Add files via upload

---
+++ modul 3 (global.).py
@@ -0,0 +1,17 @@
+calls = 0
+def count_calls():
+    global calls
+    calls += 1
+def string_info(string):
+    count_calls()
+    return(len(string), string.upper(), string.lower())
+def is_contains(string, list_to_search):
+    count_calls()
+    string_lower = string.lower()
+    return string_lower in (item.lower() for item in list_to_search)
+
+print(string_info('Capybara'))
+print(string_info('Armageddon'))
+print(is_contains('Urban', ['ban', 'baNaN', 'urBAN']))
+print(is_contains('cycle', ['recycling', 'cyclic']))
+print(calls)
