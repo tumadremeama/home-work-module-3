20be269f4a18fda8770b01e0a86b027ead5e0969
Demyan k. <demiandiaz.dd@gmail.com>
2024-09-21 09:20:56 +0300

    Update modul_3_hard.py

--- modul_3_hard.py
+++ modul_3_hard.py
@@ -2,17 +2,19 @@ def calculate_structure_sum(data):
     total_sum = 0
 
     for item in data:
-        if isinstance(item, (int, float)):
+        if isinstance(item, (int, float)):  
             total_sum += item
-        elif isinstance(item, str):
+        elif isinstance(item, str):  
             total_sum += len(item)
-        elif isinstance(item, list):
-            total_sum += calculate_structure_sum(item)
-        elif isinstance(item, dict):
-            total_sum += calculate_structure_sum(item.values())
-            total_sum += calculate_structure_sum(item.keys())
-        elif isinstance(item, tuple):
-            total_sum += calculate_structure_sum(item)
+        elif isinstance(item, list):  
+            total_sum += calculate_structure_sum(item)  
+        elif isinstance(item, dict): 
+            total_sum += calculate_structure_sum(item.values()) 
+            total_sum += calculate_structure_sum(item.keys())  
+        elif isinstance(item, tuple):  
+            total_sum += calculate_structure_sum(item)  
+        elif isinstance(item, set):  
+            total_sum += calculate_structure_sum(item)  
 
     return total_sum
 
@@ -22,8 +24,9 @@ data_structure = [
     {'a': 4, 'b': 5},
     (6, {'cube': 7, 'drum': 8}),
     "Hello",
-    ((), [{(2, 'Urban', ('Urban2', 35))}])
+    ((), [{(2, 'Urban', ('Urban2', 35))}]),
+    {9, 10, 11}  
 ]
 
 result = calculate_structure_sum(data_structure)
-print(result) # Ожиданный вывод суммы: 99
+print(result) 
